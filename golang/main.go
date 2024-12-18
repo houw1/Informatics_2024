a0948e0a71b4021b7d1f89520d566cbcec58fd22
houw1 <1>
2024-12-18 20:20:28 +0300

    Update main.go

--- golang/main.go
+++ golang/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 
+	lab4 "isuct.ru/informatics2022/Lab_4"
 	lab6 "isuct.ru/informatics2022/Lab_6"
 	lab7 "isuct.ru/informatics2022/Lab_7"
 )
 
 func main() {
+	lab4.RunLab4Tasks()
 	lab6.RunLab6()
 	lab7.RunLab7()
 	fmt.Println("Кошелев Матвей Валерьевич")
